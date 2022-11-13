4256eab3d1839f980ca62e55a0e9ecd9bd2e8f24
Akie Kido <akie.kido@gmail.com>
2022-11-13 09:02:09 -0500

    added week1.py

---
+++ week1.py
@@ -0,0 +1,33 @@
+import turtle
+def triangle(edge=100):
+    turtle.forward(edge)
+    turtle.right(120)
+    turtle.forward(edge)
+    turtle.right(120)
+    turtle.forward(edge)
+    turtle.right(120)
+    turtle.forward(edge)
+    turtle.right(120)
+
+triangle()
+triangle()
+triangle()
+triangle()
+triangle()
+triangle()
+
+def rectangle(edge=100):
+    turtle.forward(edge)
+    turtle.right(90)
+    turtle.forward(edge)
+    turtle.right(90)
+    turtle.forward(edge)
+    turtle.right(90)
+    turtle.forward(edge)
+    turtle.right(90)
+
+rectangle()
+rectangle()
+rectangle()
+rectangle()
+
